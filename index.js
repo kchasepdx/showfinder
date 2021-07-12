df0183682706e84f561f9c5b1c9d63a081de1e6d
kchasepdx <kchasepdx@gmail.com>
2021-07-12 13:14:04 -0700

    move files to root dir

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./src/App";
-import reportWebVitals from "./src/reportWebVitals";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
