c1625fb7356c17c8a79d12112e182e99afd6be6d
kchasepdx <kchasepdx@gmail.com>
2021-07-17 20:13:50 -0700

    update index.js and app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import "./App.css";
 import React from "react";
-import Nav from "./Nav";
+
 import Main from "./Main";
 
 function App() {
   return (
     <div>
-      <Nav />
       <Main />
     </div>
   );
