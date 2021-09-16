547cee4ab714a4911fd1428b15f38bd7db5dd267
kchasepdx <kchasepdx@gmail.com>
2021-09-16 14:49:48 -0700

    change homescreen background

--- src/Home.js
+++ src/Home.js
@@ -7,6 +7,10 @@ function Home() {
   return (
     <div className="App">
       <Nav />
+      <div class="bg"></div>
+      <div class="bg bg2"></div>
+      <div class="bg bg3"></div>
+
       <h1 className="app-header"> SHOW FINDER </h1>
       <p id="sub-title"> Let's find your next show.</p>
       <Image source={TV} alt="television" />
